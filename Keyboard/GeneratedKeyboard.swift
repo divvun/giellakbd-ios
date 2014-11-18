b4b76f7cf8ceea22171bf6f798af361f002af3fd
Brendan Molloy <brendan@bbqsrc.net>
2014-11-18 19:50:33 +1100

    stop errors on compiling default

--- Keyboard/GeneratedKeyboard.swift
+++ Keyboard/GeneratedKeyboard.swift
@@ -1,6 +1,29 @@
 // GENERATION STUB.
 func generatedKeyboard() -> Keyboard {
-    return defaultKeyboard()
+    var defaultKeyboard = Keyboard()
+    
+    for key in ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"] {
+        var keyModel = Key(.Character)
+        keyModel.setLetter(key)
+        defaultKeyboard.addKey(keyModel, row: 0, page: 0)
+    }
+    
+    for key in ["A", "S", "D", "F", "G", "H", "J", "K", "L"] {
+        var keyModel = Key(.Character)
+        keyModel.setLetter(key)
+        defaultKeyboard.addKey(keyModel, row: 1, page: 0)
+    }
+    
+    var keyModel = Key(.Shift)
+    defaultKeyboard.addKey(keyModel, row: 2, page: 0)
+    
+    for key in ["Z", "X", "C", "V", "B", "N", "M"] {
+        var keyModel = Key(.Character)
+        keyModel.setLetter(key)
+        defaultKeyboard.addKey(keyModel, row: 2, page: 0)
+    }
+    
+    return defaultKeyboard
 }
 
 func generatedConfig() -> [String: String] {
