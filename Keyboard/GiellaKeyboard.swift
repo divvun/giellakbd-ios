27cc5bdf752e76ace0f99536e172af02aa4bd5b2
Brendan Molloy <brendan@bbqsrc.net>
2014-12-06 02:09:03 +1100

    it's basically working

--- Keyboard/GiellaKeyboard.swift
+++ Keyboard/GiellaKeyboard.swift
@@ -94,13 +94,6 @@ class GiellaBanner: ExtraView {
     required init(globalColors: GlobalColors.Type?, darkMode: Bool, solidColorMode: Bool) {
         super.init(globalColors: globalColors, darkMode: darkMode, solidColorMode: solidColorMode)
         self.keyboard = nil
-        /*
-        self.addSubview(self.label)
-        
-        //self.label.font = UIFont(name: "ChalkboardSE-Regular", size: 22)
-        self.label.text = "Loaded."
-        self.label.sizeToFit()
-        */
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -127,64 +120,42 @@ class GiellaBanner: ExtraView {
     func applyConstraints(currentView: UIButton, prevView: UIView?, nextView: UIView?, firstView: UIView) {
         let parentView = self
         
-        /*
-        var leftConstraint: NSLayoutConstraint;
+        var leftConstraint: NSLayoutConstraint
+        var rightConstraint: NSLayoutConstraint
+        var topConstraint: NSLayoutConstraint
+        var bottomConstraint: NSLayoutConstraint
         
-        btn.setTranslatesAutoresizingMaskIntoConstraints(false)
-        
-        if first {
-            leftConstraint = NSLayoutConstraint(item: btn, attribute: .Left, relatedBy: .Equal, toItem: lastView, attribute: .Left, multiplier: 1.0, constant: 1)
-        } else if last {
-            // Actually right but let's reuse a var.
-            leftConstraint = NSLayoutConstraint(item: btn, attribute: .Right, relatedBy: .Equal, toItem: lastView, attribute: .Right, multiplier: 1.0, constant: -1)
-        } else {
-            leftConstraint = NSLayoutConstraint(item: btn, attribute: .Left, relatedBy: .Equal, toItem: lastView, attribute: .Right, multiplier: 1.0, constant: 1)
-            let widthConstraint = NSLayoutConstraint(item: btn, attribute: .Width, relatedBy: .Equal, toItem: firstView, attribute: NSLayoutAttribute.Width, multiplier: 1, constant: 0)
-            widthConstraint.priority = 800
-            
-            self.addConstraint(widthConstraint)
-        }
-        
-        let heightConstraint = NSLayoutConstraint(item: btn, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Height, multiplier: 1, constant: 0)
-        self.addConstraint(leftConstraint)
-        
-        self.addConstraint(heightConstraint)
-        */
         // Constrain to top of parent view
-        let topConstraint = NSLayoutConstraint(item: currentView, attribute: .Top, relatedBy: .Equal, toItem: parentView,
-            attribute: .Top, multiplier: 1, constant: 1)
+        topConstraint = NSLayoutConstraint(item: currentView, attribute: .Top, relatedBy: .Equal, toItem: parentView,
+            attribute: .Top, multiplier: 1.0, constant: 1)
         
-        // Constraint to bottmo of parent too
-        let bottomConstraint = NSLayoutConstraint(item: currentView, attribute: .Bottom, relatedBy: .Equal, toItem: parentView, attribute: .Bottom, multiplier: 1, constant: 1)
-        
-        addConstraints([topConstraint, bottomConstraint])
-        
-        // If first, constrain to left of parent
-        if prevView == nil {
-            let leftConstraint = NSLayoutConstraint(item: currentView, attribute: .Left, relatedBy: .Equal, toItem: parentView, attribute: .Left, multiplier: 1, constant: 1)
-            addConstraint(leftConstraint)
-        }
+        // Constraint to bottom of parent too
+        bottomConstraint = NSLayoutConstraint(item: currentView, attribute: .Bottom, relatedBy: .Equal, toItem: parentView, attribute: .Bottom, multiplier: 1.0, constant: -1)
         
         // If last, constrain to right
         if nextView == nil {
-            let rightConstraint = NSLayoutConstraint(item: currentView, attribute: .Right, relatedBy: .Equal, toItem: parentView, attribute: .Right, multiplier: 1, constant: 1)
-            addConstraint(rightConstraint)
+            rightConstraint = NSLayoutConstraint(item: currentView, attribute: .Right, relatedBy: .Equal, toItem: parentView, attribute: .Right, multiplier: 1.0, constant: -1)
+        } else {
+            rightConstraint = NSLayoutConstraint(item: currentView, attribute: .Right, relatedBy: .Equal, toItem: nextView, attribute: .Left, multiplier: 1.0, constant: -1)
         }
         
-        // Constrain to previous button if all is well, and set same width
-        if nextView != nil && prevView != nil {
-            let rightConstraint = NSLayoutConstraint(item: currentView, attribute: .Right, relatedBy: .Equal, toItem: nextView, attribute: .Left, multiplier: 1, constant: -1)
+        // If first, constrain to left of parent
+        if prevView == nil {
+            leftConstraint = NSLayoutConstraint(item: currentView, attribute: .Left, relatedBy: .Equal, toItem: parentView, attribute: .Left, multiplier: 1.0, constant: 1)
+        } else {
+            leftConstraint = NSLayoutConstraint(item: currentView, attribute: .Left, relatedBy: .Equal, toItem: prevView, attribute: .Right, multiplier: 1.0, constant: 1)
             
-            let leftConstraint = NSLayoutConstraint(item: currentView, attribute: .Left, relatedBy: .Equal, toItem: prevView, attribute: .Right, multiplier: 1, constant: 1)
-            
-            let widthConstraint = NSLayoutConstraint(item: firstView, attribute: .Width, relatedBy: .Equal, toItem: currentView, attribute: .Width, multiplier: 1, constant: 0)
+            let widthConstraint = NSLayoutConstraint(item: firstView, attribute: .Width, relatedBy: .Equal, toItem: currentView, attribute: .Width, multiplier: 1.0, constant: 0)
             
             widthConstraint.priority = 800
             
-            addConstraints([rightConstraint, leftConstraint, widthConstraint])
+            addConstraint(widthConstraint)
         }
+        
+        addConstraints([topConstraint, bottomConstraint, rightConstraint, leftConstraint])
+        
     }
-
+    
     
     func updateAlternateKeyList(keys: [String]) {
         var sv = self.subviews
@@ -192,13 +163,25 @@ class GiellaBanner: ExtraView {
             v.removeFromSuperview()
         }
         
-        var lastN = keys.count-1
-        var prevBtn: UIButton?
-        var nextBtn: UIButton?
+        if keys.count == 0 {
+            return
+        }
         
         for char in keys {
             var btn: UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
-            btn.setTitle(char, forState: UIControlState.Normal)
+            
+            btn.frame = CGRectMake(0, 0, 20, 20)
+            btn.setTitle(char, forState: .Normal)
+            btn.sizeToFit()
+            btn.titleLabel?.font = UIFont.systemFontOfSize(18)
+            
+            
+            btn.setTranslatesAutoresizingMaskIntoConstraints(false)
+            btn.backgroundColor = UIColor(hue: (216/360.0), saturation: 0.1, brightness: 0.81, alpha: 1)
+            btn.setTitleColor(UIColor(white: 1.0, alpha: 1.0), forState: .Normal)
+            
+            btn.setContentHuggingPriority(1000, forAxis: .Horizontal)
+            btn.setContentCompressionResistancePriority(1000, forAxis: .Horizontal)
             
             btn.addTarget(self, action: Selector("handleBtnPress:"), forControlEvents: .TouchUpInside)
             
@@ -206,6 +189,9 @@ class GiellaBanner: ExtraView {
         }
         
         let firstBtn = self.subviews[0] as UIButton
+        var lastN = keys.count-1
+        var prevBtn: UIButton?
+        var nextBtn: UIButton?
         
         for (n, view) in enumerate(self.subviews) {
             let btn = view as UIButton
@@ -216,9 +202,13 @@ class GiellaBanner: ExtraView {
                 nextBtn = self.subviews[n+1] as? UIButton
             }
             
-            applyConstraints(btn, prevView: prevBtn, nextView: nextBtn, firstView: firstBtn)
+            if n == 0 {
+                prevBtn = nil
+            } else {
+                prevBtn = self.subviews[n-1] as? UIButton
+            }
             
-            prevBtn = btn
+            applyConstraints(btn, prevView: prevBtn, nextView: nextBtn, firstView: firstBtn)
         }
     }
 }
