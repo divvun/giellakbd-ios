f0611eb6a0546192aa575be27c9eb1cb538eaae2
Dylan Hand <dylan.hand@gmail.com>
2020-01-23 09:50:00 +0100

    Do setup at init, bannerView is optional
    This is part of what fixes the keyboard height glitching.

--- Keyboard/EntryPoint.swift
+++ Keyboard/EntryPoint.swift
@@ -2,10 +2,17 @@ import BaseKeyboard
 
 class EntryKeyboard: KeyboardViewController {
     private var bannerPlugin: DivvunSpellBannerPlugin!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(withBanner: true)
+        bannerPlugin = DivvunSpellBannerPlugin(keyboard: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        bannerPlugin = DivvunSpellBannerPlugin(keyboard: self)
     }
 }
