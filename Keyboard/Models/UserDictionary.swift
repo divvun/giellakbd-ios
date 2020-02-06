5f3040f1f79e59eaa7ce4ed558588336293eed54
Dylan Hand <dylan.hand@gmail.com>
2020-02-06 15:53:57 +0100

    Update column names to match index value

--- Keyboard/Models/UserDictionary.swift
+++ Keyboard/Models/UserDictionary.swift
@@ -4,9 +4,9 @@ import SQLite
 public class UserDictionary {
     private let userDictionary = Table("UserDictionary")
     private let id = Expression<Int64>("id")
-    private let word1Col = Expression<String>("word1")
+    private let word0Col = Expression<String>("word0")
+    private let word1Col = Expression<String?>("word1")
     private let word2Col = Expression<String?>("word2")
-    private let word3Col = Expression<String?>("word3")
     private let userWordIndexCol = Expression<Int64>("user_word_index")
 
     private lazy var dbFilePath: String = {
@@ -32,9 +32,9 @@ public class UserDictionary {
         do {
             try database.run(userDictionary.create(ifNotExists: true) { table in
                 table.column(id, primaryKey: true)
+                table.column(word0Col)
                 table.column(word1Col)
                 table.column(word2Col)
-                table.column(word3Col)
                 table.column(userWordIndexCol)
             })
         } catch {
@@ -45,27 +45,27 @@ public class UserDictionary {
     public func addTestRows() {
         let names = ["Captain", "Wishbone", "Salad", "Dirk"]
         for personName in names {
-            add(word1: personName, word2: "other", userWordIndex: 1)
+            add(word0: personName, word1: "other", word2: "other2", userWordIndex: 0)
         }
     }
 
-    public func add(word1: String, word2: String? = nil, word3: String? = nil, userWordIndex: Int = 0) {
+    public func add(word0: String, word1: String? = nil, word2: String? = nil, userWordIndex: Int = 0) {
         guard userWordIndex >= 0 else {
             fatalError("Attempted to add word to UserDictionary with below-zero index")
         }
-        if word3 != nil && word2 == nil {
+        if word2 != nil && word1 == nil {
             let message = "Attempted to add non-nil word3 with nil word2 to UserDictionary."
                 + " Ensure word2 is non-nil before adding a word3."
             fatalError(message)
         }
-        if word3 == nil && userWordIndex > 1
-            || word2 == nil && userWordIndex > 0 {
+        if word2 == nil && userWordIndex > 1
+            || word1 == nil && userWordIndex > 0 {
             fatalError("Attempted to add word to UserDictionary with invalid word index. userWordIndex: \(userWordIndex)")
         }
 
-        let insert = userDictionary.insert(word1Col <- word1,
+        let insert = userDictionary.insert(word0Col <- word0,
+                                           word1Col <- word1,
                                            word2Col <- word2,
-                                           word3Col <- word3,
                                            userWordIndexCol <- Int64(userWordIndex))
         do {
             try database.run(insert)
@@ -78,9 +78,9 @@ public class UserDictionary {
         do {
             for row in try database.prepare(userDictionary) {
                 let rowData = "id: \(row[id]), "
+                    + "word0: \(String(describing: row[word0Col])), "
                     + "word1: \(String(describing: row[word1Col])), "
                     + "word2: \(String(describing: row[word2Col])), "
-                    + "word3: \(String(describing: row[word3Col])), "
                     + "index: \(row[userWordIndexCol])"
                 print(rowData)
             }
