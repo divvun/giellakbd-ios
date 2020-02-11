84aa1eaf83c0bef9d2a61c870ad1bf3874db3bbd
Dylan Hand <dylan.hand@gmail.com>
2020-02-11 10:14:11 +0100

    Refresh hosting app when keyboard enabled/disabled

--- HostingApp/Controllers/HomeController.swift
+++ HostingApp/Controllers/HomeController.swift
@@ -20,8 +20,12 @@ class HomeController: ViewController<HomeView>, HideNavBar {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        contentView.configStack?.isHidden = AppDelegate.instance.isKeyboardEnabled
+        refreshUI()
         contentView.awakeFromNib()
+    }
+
+    @objc private func refreshUI() {
+        contentView.configStack?.isHidden = AppDelegate.instance.isKeyboardEnabled
     }
 
     override func viewDidLoad() {
@@ -29,10 +33,19 @@ class HomeController: ViewController<HomeView>, HideNavBar {
 
         title = Strings.localizedName
 
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(refreshUI),
+                                               name: UIApplication.didBecomeActiveNotification,
+                                               object: nil)
+
         contentView.langButton.addTarget(self, action: #selector(openLanguages), for: [.touchUpInside])
         contentView.langButton2.addTarget(self, action: #selector(openLanguages), for: [.touchUpInside])
         contentView.helpButton.addTarget(self, action: #selector(openInstructions), for: [.touchUpInside])
         contentView.aboutButton.addTarget(self, action: #selector(openAbout), for: [.touchUpInside])
         contentView.testingButton.addTarget(self, action: #selector(openTesting), for: [.touchUpInside])
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
