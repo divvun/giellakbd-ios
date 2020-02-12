b9b1a7b0f5f6d2f17f2a913296e4e2dd29410baa
Dylan Hand <dylan.hand@gmail.com>
2020-02-12 09:44:43 +0100

    User Dictionary view uses auto layout in nib

--- HostingApp/Views/UserDictionaryView.swift
+++ HostingApp/Views/UserDictionaryView.swift
@@ -2,12 +2,4 @@ import UIKit
 
 class UserDictionaryView: UIView, Nibbable {
     @IBOutlet var tableView: UITableView!
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-        // For some reason it's not possible to set auto layout constraints on a table view in nib
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.fill(superview: self)
-    }
 }
