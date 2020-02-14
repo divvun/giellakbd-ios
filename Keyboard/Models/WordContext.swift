5826951266811098a6de6e81a2fe48b813cc3f49
Dylan Hand <dylan.hand@gmail.com>
2020-02-14 13:35:43 +0100

    Remove ellipses and punctuation from context string

--- Keyboard/Models/WordContext.swift
+++ Keyboard/Models/WordContext.swift
@@ -6,19 +6,6 @@ public struct WordContext {
     let word2: String?
     let userWord: String
 
-    private var userWordIsFirstWord: Bool {
-        word0 == userWord
-    }
-
-    private var userWordIsLastWord: Bool {
-        if let word2 = word2 {
-            return word2 == userWord
-        } else if let word1 = word1 {
-            return word1 == userWord
-        }
-        return true
-    }
-
     public func contextAttributedString() -> NSAttributedString {
         var contextString = word0
         if let word1 = word1 {
@@ -26,16 +13,6 @@ public struct WordContext {
         }
         if let word2 = word2 {
             contextString += " \(word2)"
-        }
-
-        if !userWordIsFirstWord {
-            contextString = "... " + contextString
-        }
-
-        if userWordIsLastWord {
-            contextString += "."
-        } else {
-            contextString += " ..."
         }
 
         return contextString.bolden(substring: userWord, caseInsensitive: true)
