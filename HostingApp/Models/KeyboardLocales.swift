892e86e7b41cba63e5fb7a53cf98305ad4a243f2
Dylan Hand <dylan.hand@gmail.com>
2020-02-18 10:17:38 +0100

    Add KeyboardLocales model

---
+++ HostingApp/Models/KeyboardLocales.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+struct KeyboardLocale {
+    let identifier: String
+    let langaugeName: String
+}
+
+class KeyboardLocales {
+    static var allLocales: [KeyboardLocale] {
+        var locales: [KeyboardLocale] = []
+        let appexBundles = Bundle.allBundles.filter({ (bundle) -> Bool in
+            bundle.bundleURL.pathExtension == "appex"
+        })
+
+        for bundle in appexBundles {
+            guard let info = bundle.infoDictionary,
+                let languageName = info["CFBundleDisplayName"] as? String,
+                let ext = info["NSExtension"] as? [String: Any],
+                let attributes = ext["NSExtensionAttributes"] as? [String: Any],
+                let languageIdentifier = attributes["PrimaryLanguage"] as? String else {
+                continue
+            }
+            let locale = KeyboardLocale(identifier: languageIdentifier, langaugeName: languageName)
+            locales.append(locale)
+        }
+        return locales
+    }
+}
