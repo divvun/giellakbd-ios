5cac8f346fd10921f464988314a9e42d68115d7f
Dylan Hand <dylan.hand@gmail.com>
2020-02-18 14:37:34 +0100

    Add more user dictionary tests

--- HostingAppTests/HostingAppTests.swift
+++ HostingAppTests/HostingAppTests.swift
@@ -2,9 +2,14 @@
 import XCTest
 
 class UserDictionaryTests: XCTestCase {
+    let userDictionary = UserDictionary()
+
+    override func setUp() {
+        userDictionary.deleteAllWords()
+    }
 
     func test_delete_database_removes_all_words() {
-        let sut = UserDictionary()
+        let sut = userDictionary
 
         for _ in 0...5 {
             // add multple times to be sure it becomes a "user word"
@@ -17,4 +22,36 @@ class UserDictionaryTests: XCTestCase {
         XCTAssertEqual(0, rows.count)
     }
 
+    func test_user_words_are_case_insensitive() {
+        let sut = userDictionary
+
+        sut.add(word0: "test")
+        sut.add(word0: "TEST")
+        sut.add(word0: "Test")
+
+        let words = sut.getUserWords()
+        XCTAssertEqual(1, words.count)
+        XCTAssertEqual("test", words.first)
+    }
+
+    func test_word_added_once_does_not_count_as_user_word() {
+        let sut = userDictionary
+
+        sut.add(word0: "test")
+
+        let words = sut.getUserWords()
+        XCTAssertEqual(0, words.count)
+    }
+
+    func test_word_added_twice_does_count_as_user_word() {
+        let sut = userDictionary
+        let word = "test"
+
+        sut.add(word0: word)
+        sut.add(word0: word)
+
+        let words = sut.getUserWords()
+        XCTAssertEqual(1, words.count)
+        XCTAssertEqual(word, words.first)
+    }
 }
