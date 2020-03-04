5a3b10408fedd646eab9797667aaf9e2d6fedead
Dylan Hand <dylan.hand@gmail.com>
2020-03-04 13:29:06 +0100

    Add better tests for isLeftShiftedVariationOf
    It looks like we're not going to need this method at all, but adding now because they were useful when debugging.

--- HostingAppTests/WordContextTests.swift
+++ HostingAppTests/WordContextTests.swift
@@ -29,12 +29,57 @@ class WordContextTests: XCTestCase {
     }
 
     func test_is_left_shifted_variation_of_should_return_true_when_context_is_equal_but_shifted() {
-        XCTAssertTrue(WordContext(firstBefore: "hi", word: "test")
-            .isLeftShiftedVariationOf(WordContext(word: "hi")))
-        XCTAssertTrue(WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
-            .isLeftShiftedVariationOf(WordContext(firstBefore: "hey", word: "hi")))
-        XCTAssertTrue(WordContext(secondBefore: "hi", firstBefore: "test", word: "foo")
-            .isLeftShiftedVariationOf(WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")))
+        let context1 = WordContext(word: "hi")
+        let context1Shifted = WordContext(firstBefore: "hi", word: "test")
+
+        XCTAssertTrue(context1Shifted.isLeftShiftedVariationOf(context1))
+
+        let context2 = WordContext(firstBefore: "hey", word: "hi")
+        let context2Shifted = WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
+
+        XCTAssertTrue(context2Shifted.isLeftShiftedVariationOf(context2))
+
+        let context3 = WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
+        let context3Shifted = WordContext(secondBefore: "hi", firstBefore: "test", word: "foo")
+
+        XCTAssertTrue(context3Shifted.isLeftShiftedVariationOf(context3))
+
+        let context4 = WordContext(word: "hi")
+        let context4Shifted = WordContext(firstBefore: "hi", word: "")
+
+        XCTAssertTrue(context4Shifted.isLeftShiftedVariationOf(context4))
+    }
+
+    func test_is_left_shifted_variation_of_should_return_false_when_context_not_equal_or_not_shifted() {
+        let context1 = WordContext(word: "hi")
+        let context1Shifted = WordContext(word: "hi")
+
+        XCTAssertFalse(context1Shifted.isLeftShiftedVariationOf(context1))
+
+        let context2 = WordContext(word: "hi")
+        let context2Shifted = WordContext(firstBefore: "test", word: "hi")
+
+        XCTAssertFalse(context2Shifted.isLeftShiftedVariationOf(context2))
+
+        let context3 = WordContext(firstBefore: "hey", word: "hi")
+        let context3Shifted = WordContext(secondBefore: "WRONG", firstBefore: "hi", word: "test")
+
+        XCTAssertFalse(context3Shifted.isLeftShiftedVariationOf(context3))
+
+        let context4 = WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
+        let context4Shifted = WordContext(secondBefore: "hi", firstBefore: "WRONG", word: "foo")
+
+        XCTAssertFalse(context4Shifted.isLeftShiftedVariationOf(context4))
+
+        let context5 = WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
+        let context5Shifted = WordContext(secondBefore: nil, firstBefore: "test", word: "foo")
+
+        XCTAssertFalse(context5Shifted.isLeftShiftedVariationOf(context5))
+
+        let context6 = WordContext(secondBefore: "hey", firstBefore: "hi", word: "test")
+        let context6Shifted = WordContext(secondBefore: "hi", firstBefore: nil, word: "foo")
+
+        XCTAssertFalse(context6Shifted.isLeftShiftedVariationOf(context6))
     }
 
     func test_combining_contexts_should_work() {
