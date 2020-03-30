49bf564d1c0e964e0fdbf48346740fb366fd95e3
Dylan Hand <dylan.hand@gmail.com>
2020-03-30 11:37:46 +0200

    Use convenience var for getting group container URL

--- HostingApp/KeyboardSettings.swift
+++ HostingApp/KeyboardSettings.swift
@@ -11,6 +11,13 @@ final class KeyboardSettings {
         return "group.\(Bundle.top.bundleIdentifier!)"
     }()
 
+    static var groupContainerURL: URL = {
+        guard let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupId) else {
+            fatalError("Error opening app group for group id: \(groupId)")
+        }
+        return url
+    }()
+
     static var languageCode: String {
         get { return defaults.string(forKey: "language") ?? Locale.current.languageCode! }
         set { defaults.set(newValue, forKey: "language") }
