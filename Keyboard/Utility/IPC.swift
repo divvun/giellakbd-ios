b0a6ee3daa6664f3455922f15bce3edf7b3399ef
Dylan Hand <dylan.hand@gmail.com>
2020-03-30 17:21:38 +0200

    IPC always writes to existing file
    Tried writing to a file that always exists, as opposed to deleting and creating a file, in hopes that it would be more reliable. It's not reliable either.

--- Keyboard/Utility/IPC.swift
+++ Keyboard/Utility/IPC.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+fileprivate let trueStr = "1"
+fileprivate let falseStr = "0"
+
 protocol IPCDelegate: class {
     func didBeginDownloadingUpdate()
     func didFinishDownloadingUpdate()
@@ -11,16 +14,17 @@ final class IPC {
     private let ipcDirectory = KeyboardSettings.groupContainerURL.appendingPathComponent("ipc", isDirectory: true)
     private lazy var isDownloadingFile = ipcDirectory.appendingPathComponent("isDownloading")
 
+    private let trueData = trueStr.data(using: .utf8)!
+    private let falseData = falseStr.data(using: .utf8)!
+
     public var isDownloading: Bool {
         set {
-            if newValue == true {
-                createIsDownloadingFile()
-            } else {
-                removeIsDownloadingFile()
+            if isDownloading != newValue {
+                tryToWrite(isDownloading: newValue)
             }
         }
         get {
-            fileExists(atUrl: isDownloadingFile)
+            tryToReadIsDownloading()
         }
     }
 
@@ -36,16 +40,34 @@ final class IPC {
         do {
             fileWatcher = try SKQueue(delegate: self)
             fileWatcher?.addPath(ipcDirectory.path) // kqueue requires adding the containing directory before adding the file of interest
-//            fileWatcher?.addPath(isDownloadingFile.path)
+            fileWatcher?.addPath(isDownloadingFile.path)
         } catch {
             fatalError("Error creating SKQueue: \(error)")
+        }
+    }
+
+    private func tryToWrite(isDownloading: Bool) {
+        do {
+            let data = isDownloading ? trueData : falseData
+            try data.write(to: isDownloadingFile)
+        } catch {
+            fatalError("Error writing isDownloading file: \(error)")
+        }
+    }
+
+    private func tryToReadIsDownloading() -> Bool {
+        do {
+            let str = try String(contentsOf: isDownloadingFile, encoding: .utf8)
+            return str == trueStr
+        } catch {
+            fatalError("Error reading isDownloading file: \(error)")
         }
     }
 
     private func createIsDownloadingFile() {
         createIpcDirectoryIfNeeded()
         if !fileExists(atUrl: isDownloadingFile) {
-            FileManager.default.createFile(atPath: isDownloadingFile.path, contents: nil, attributes: nil)
+            FileManager.default.createFile(atPath: isDownloadingFile.path, contents: falseData, attributes: nil)
         }
     }
 
@@ -75,10 +97,14 @@ extension IPC: SKQueueDelegate {
     func receivedNotification(_ notification: SKQueueNotification, path: String, queue: SKQueue) {
         print(notification)
         print(path)
-        if fileExists(atUrl: isDownloadingFile) {
-            delegate?.didBeginDownloadingUpdate()
-        } else {
-            delegate?.didFinishDownloadingUpdate()
+        if path == isDownloadingFile.path {
+            if notification == .write {
+                if isDownloading {
+                    delegate?.didBeginDownloadingUpdate()
+                } else {
+                    delegate?.didFinishDownloadingUpdate()
+                }
+            }
         }
     }
 }
