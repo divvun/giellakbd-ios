b5e8bb99deaa3fe286bf1e64b911dcf3c4d1787a
Dylan Hand <dylan.hand@gmail.com>
2020-04-08 14:05:38 +0200

    Remove IPCDelegate from UpdateBanner

--- Keyboard/Banner Plugins/Update Banner/UpdateBanner.swift
+++ Keyboard/Banner Plugins/Update Banner/UpdateBanner.swift
@@ -2,7 +2,6 @@ import Foundation
 
 final class UpdateBanner: Banner {
     private let bannerView: UpdateBannerView
-    private let ipc = IPC()
 
     var view: UIView {
         bannerView
@@ -10,20 +9,9 @@ final class UpdateBanner: Banner {
 
     init(theme: ThemeType) {
         bannerView = UpdateBannerView(theme: theme)
-        ipc.delegate = self
     }
 
     func updateTheme(_ theme: ThemeType) {
         bannerView.updateTheme(theme)
-    }
-}
-
-extension UpdateBanner: IPCDelegate {
-    func didBeginDownloading(id: String) {
-        bannerView.text = "Downloading \(id)"
-    }
-
-    func didFinishInstalling(id: String) {
-        bannerView.text = "Finished installing \(id)"
     }
 }
