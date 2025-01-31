7b4d71c0d02fa9b14a3aa2b7bc0a141d678ec8af
Dylan Hand <dylan.hand@gmail.com>
2025-01-31 16:04:05 +0100

    Make sure the English version of keyboardNotSupported message is shown

--- Keyboard/Controllers/KeyboardViewController.swift
+++ Keyboard/Controllers/KeyboardViewController.swift
@@ -283,7 +283,7 @@ open class KeyboardViewController: UIInputViewController {
         setupKeyboardContainer()
 
         let notSupportedLabel = UILabel()
-        notSupportedLabel.text = String(format: NSLocalizedString("keyboardNotSupported", comment: ""), keyboardDefinition.name)
+        notSupportedLabel.text = String(format: NSLocalizedString("The %@ keyboard is currently not supported on this device.\nYou can submit a request for support with one of the following options:", comment: ""), keyboardDefinition.name)
         notSupportedLabel.textAlignment = .center
         notSupportedLabel.lineBreakMode = .byWordWrapping
         notSupportedLabel.numberOfLines = 0
