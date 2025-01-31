9901fc42e6d1762a188d0cd178a0018c2e84ff23
Dylan Hand <dylan.hand@gmail.com>
2025-01-31 16:36:55 +0100

    Fix GitHub issue formatting to work with GitHub iOS App

--- Keyboard/Controllers/KeyboardViewController.swift
+++ Keyboard/Controllers/KeyboardViewController.swift
@@ -729,7 +729,7 @@ open class KeyboardViewController: UIInputViewController {
     @objc private func githubIssueButtonTapped() {
         let deviceName = DeviceVariant.from(traits: self.traitCollection).displayName()
         let repo = "keyboard-\(keyboardDefinition.locale)"
-        let coded = "https://github.com/giellalt/\(repo)/issues/new?labels=enhancement&title=Add+Support+for+\(deviceName)&body=Additional+notes+(optional):\n\n"
+        let coded = "https://github.com/giellalt/\(repo)/issues/new?labels=enhancement&title=Add Support for \(deviceName)&body=Additional notes (optional):\n\n"
 
         guard let escaped = coded.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
               let url = URL(string: escaped) else {
